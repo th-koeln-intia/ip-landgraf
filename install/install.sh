26a59c49307a00351bc4f1cef4b95952556b651c
Eric L <eric.landgraf@smail.th-koeln.de>
2021-10-22 21:43:06 +0200

    refine install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,34 +1,30 @@
+
 if [[ "$EUID" -ne 0 ]]
-  then echo "Bitte als "root" ausführen"
+  then echo "Bitte als "root" ausführen! ("sudo ./install.sh")"
   exit
 fi
 
-apt update -y
+apt update -y 
 apt upgrade -y
-
-apt-get install -y git curl python3 python3-pip npm 
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt install -y git curl npm nodejs 
 curl -sSL https://get.docker.com | sh
 usermod -aG docker $USER
 pip3 -v install docker-compose
-git clone https://github.com/respeaker/seeed-voicecard.git
-cd seeed-voicecard
-./install.sh 
-cd ..
 git clone https://github.com/th-koeln-intia/ip-landgraf.git ./maxmax-git
 mkdir ./MaxMax
 cp -R ./maxmax-git/config/. ./MaxMax
 rm -R ./maxmax-git
 chmod 777 -R ./MaxMax
 cd ./MaxMax/node-red/data
-npm install --unsafe-perm
+npm i -g n
+n stable
+bash -lic "npm i"
+apt -y autoremove
 cd ..
 cd ..
 cd ..
-sudo apt autoremove
-
-echo "Bitte den Raspberry Pi neustarten"
-
-
-
+git clone https://github.com/respeaker/seeed-voicecard.git
+cd seeed-voicecard
+./install.sh 
+echo "Bitte den Raspberry Pi neustarten! ("sudo reboot")"
+reboot
